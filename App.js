bab3fbb44b4df833c10e44f397b80df8eebb4cc4
AntonGorban <antogor.work@gmail.com>
2021-02-22 19:07:31 +0300

    conect fonts

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 
 const variables = {
   color: {
@@ -11,10 +12,48 @@ const variables = {
 };
 
 export default function App() {
+  const [fontsLoaded] = Font.useFonts({
+    "Montserrat-Alternates-light": require("./assets/fonts/MontserratAlternates-Light.otf"),
+    "Montserrat-Alternates-regular": require("./assets/fonts/MontserratAlternates-Regular.otf"),
+    "Montserrat-Alternates-bold": require("./assets/fonts/MontserratAlternates-Bold.otf"),
+    "Fredericka-the-Great": require("./assets/fonts/FrederickatheGreat-Regular.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text> Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Platform Default</Text>
+      <Text
+        style={{ ...styles.text, fontFamily: "Montserrat-Alternates-light" }}
+      >
+        Montserrat Alternates light
+      </Text>
+      <Text
+        style={{ ...styles.text, fontFamily: "Montserrat-Alternates-regular" }}
+      >
+        Montserrat Alternates regular
+      </Text>
+      <Text
+        style={{ ...styles.text, fontFamily: "Montserrat-Alternates-bold" }}
+      >
+        Montserrat Alternates bold
+      </Text>
+      <Text
+        style={{
+          ...styles.text,
+          fontFamily: "Fredericka-the-Great",
+        }}
+      >
+        Fredericka the Great
+      </Text>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="rgba(50, 25, 200, 0.7)"
+        // hidden={true}
+      />
     </View>
   );
 }
@@ -25,5 +64,12 @@ const styles = StyleSheet.create({
     backgroundColor: variables.color.lightBlack,
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 15,
+  },
+  text: {
+    fontSize: 35,
+    textAlign: "center",
+    paddingVertical: 15,
+    color: variables.color.white,
   },
 });
